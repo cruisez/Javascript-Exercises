e33aa9247c2cd45f82667851432912c31e31e210
soorya <sooryaghanesh@gmail.com>
2023-07-22 13:38:48 +0530

    Add ex2.2.js

---
+++ chapter_2/ex2.2.js
@@ -0,0 +1,7 @@
+let myname = "Soorya";
+let age = 31;
+let Javascript = true;
+let message = "Hello, my name is " + myname +",  I am " + age + " years old and i can code Javascript: " + Javascript +".";
+
+//console.log(message);
+console.log("Hello, my name is", myname,"I am", age, "years old and i can code Javascript =", Javascript);
