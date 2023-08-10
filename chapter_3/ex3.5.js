36b26468716ed40ca187d0e243118d7bb175e9da
soorya <sooryaghanesh@gmail.com>
2023-08-11 00:34:03 +0530

    Add description and comment

--- chapter_3/ex3.5.js
+++ chapter_3/ex3.5.js
@@ -1,3 +1,9 @@
+/*
+1. Create an object named people that contains an empty array that is called friends.
+2. Create three variables, each containing an object, that contain one of your friend's first names, last names, and an ID value.
+3. Add the three friends to the friend array.
+4. Output it to the console.*/
+
 let people = {
   friends: []
 };
@@ -18,5 +24,5 @@ friend3 = {
 };
 
 
-people.friends.push(friend1, friend2, friend3);
+people.friends.push(friend1, friend2, friend3); // Using push method to add an object to an array.
 console.log(people);
