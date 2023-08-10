56dcee8521761edab1423d31df95dd6a07bfcce4
soorya <sooryaghanesh@gmail.com>
2023-08-11 00:25:59 +0530

    Add ex3.5

---
+++ chapter_3/ex3.5.js
@@ -0,0 +1,22 @@
+let people = {
+  friends: []
+};
+friend1 = {
+  firstName: "John",
+  lastName: "Connor",
+  ID: 123,
+};
+friend2 = {
+  firstName: "Tom",
+  lastName: "Cruise",
+  ID: 456,
+};
+friend3 = {
+  firstName: "Sarah",
+  lastName: "Connor",
+  ID: 789,
+};
+
+
+people.friends.push(friend1, friend2, friend3);
+console.log(people);
