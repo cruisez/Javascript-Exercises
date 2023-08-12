d559c6fc9d7b9595e725335b1ed9af4d80a3771f
soorya <sooryaghanesh@gmail.com>
2023-08-13 00:13:31 +0530

    Add ex4.3.js

---
+++ chapter_4/ex4.3.js
@@ -0,0 +1,9 @@
+/*
+Create a Boolean value for an ID variable
+Using a ternary operator, create a message variable that will check whether their ID is valid and either allow a person into a venue or not
+Output the response to the console
+*/
+
+let ID = false;
+
+ID === true ? console.log("Valid ID"): console.log("Invalid ID");
