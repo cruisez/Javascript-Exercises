26d8f0c04b687fbda064287b23c6dd07fc5462b3
Soorya <sooryaghanesh@gmail.com>
2023-11-25 00:18:51 +0530

    Minor adjustment in ex2.2.js

--- chapter_2/ex2.2.js
+++ chapter_2/ex2.2.js
@@ -4,4 +4,4 @@ let Javascript = true;
 let message = "Hello, my name is " + myname +",  I am " + age + " years old and i can code Javascript: " + Javascript +".";
 
 //console.log(message);
-console.log("Hello, my name is", myname,"I am", age, "years old and i can code Javascript =", Javascript);
+console.log(message);
